41e2fd3dedfb3e4501581df72fb12bfadc6610bf
Wiktor Kościelny <81425551+wiktorkoscielny@users.noreply.github.com>
2022-07-01 21:57:20 +0200

    Created html form with action

--- index.php
+++ index.php
@@ -6,13 +6,15 @@
 </head>
 
 <body>
-
-<?php
-	$name = "Joanna";
-	echo "Welcome ".$name;
-	echo "Welcome $name";
-	echo $name." Welcome!";
-?>
+	<h1>Online order</h1>
+	<form action='order.php'>
+		How many donuts (2 $/pcs): <input type='text' />
+		<br /><br />
+		How many bread rolls (1.5 $/pcs): <input type='text' />
+		<br /><br />
+		<input type='submit' value='Send order' />
+	</form>
+	
 	
 </body>
 </html>
